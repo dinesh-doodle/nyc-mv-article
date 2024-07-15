43422664c67e6e0262cb2d88f05cc360cdf5613f
Dinesh <v-dineshkumaar.k@yatra.com>
2024-07-15 20:58:22 +0530

    Updates

--- src/component/Loader/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./style.module.scss";
-
-export function Loader(){
-    return (
-        <div className={styles.Wrapper}>
-            <div className={styles.customLoader}></div>
-        </div>
-    )
-}
